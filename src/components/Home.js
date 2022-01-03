4c17c0b9e3ee0d32f4bc03316ee441d26e6f5507
Tami Olafunmiloye <tamiolaf@gmail.com>
2022-01-03 15:11:24 -0500

    test change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,43 @@
-import profile_pic from '../img/profile-pic.jpeg'
+import Pronunciation from './Pronunciation';
+import profile_pic from '../img/profile-pic.jpeg';
 import ImageFadeIn from "react-image-fade-in";
+import '../scss/Home.scss';
 
-function Home() {
+const Home = () => {
 
 return (
-    <div className="App">
-        <main className="App-main">
-            <ImageFadeIn src={profile_pic} alt={'Tami Olafunmiloye'} className="profile-pic"/>
-            <div style={{ backgroundColor: '#fff', marginTop: '.5rem', padding: '.5rem', border: '.1875rem solid black' }}>Hi, I'm Tami Olafunmiloye.</div>
-            <div style={{ backgroundColor: '#fff', marginTop: '.75rem', padding: '.5rem', border: '.1875rem solid black' }}>I'm a full stack engineer with experience in finance and media!</div>
-            <div style={{ backgroundColor: '#fff', marginTop: '.75rem', padding: '.5rem .75rem', border: '.1875rem solid black' }}>
-                <a style={{ textDecoration: 'none', color: 'black' }} href="https://www.github.com/tamiolaf"> <i className="fab fa-github-square fa-3x"></i></a>
-                <a style={{ textDecoration: 'none', color: 'black' }} href="https://www.linkedin.com/in/tamiolaf"> <i className="fab fa-linkedin fa-3x"></i></a>
-                <a style={{ textDecoration: 'none', color: 'black' }} href="https://www.twitter.com/tamiolaf"> <i className="fab fa-twitter-square fa-3x"></i></a>
-                <a style={{ textDecoration: 'none', color: 'black' }} href="mailto:tamiolaf@gmail.com"> <i className="fas fa-envelope-square fa-3x"></i></a>
-            </div>
-        </main>
-    </div>
-    )
+  <div className="home">
+    <main className="home_main">
+      <ImageFadeIn src={profile_pic} alt={'Tami Olafunmiloye'} className="profile-pic"/>
+      
+      <div className="app_box-text">Hi, I'm Tami Olafunmiloye! <Pronunciation/></div>
+      <div className="app_box-text">
+        I'm a full stack engineer with experience in finance and media! Check out my latest <a className="home_link" href="https://drive.google.com/file/d/1rQhSaJow16yhyrydc6dTgVXeZ4yILL2Q/view?usp=sharing">tech talk</a>.
+      </div>
+      
+      <ul className="home_box-icon">
+        <li>
+        <a className="icon" href="https://www.github.com/tamiolaf">
+          <i className="fab fa-github-square fa-3x"></i>
+        </a>
+        </li>
+        <li> <a className="icon" href="https://www.linkedin.com/in/tamiolaf">
+          <i className="fab fa-linkedin fa-3x"></i>
+        </a></li>
+        <li>
+        <a className="icon" href="https://www.twitter.com/tamiolaf">
+          <i className="fab fa-twitter-square fa-3x"></i>
+        </a>
+        </li>
+        <li>
+          <a className="icon" href="mailto:tamiolaf@gmail.com">
+            <i className="fas fa-envelope-square fa-3x"></i>
+          </a>
+        </li>
+      </ul>
+    </main>
+  </div>
+  )
 }
 
 export default Home;
