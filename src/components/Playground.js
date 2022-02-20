73e62f90f28a22dc06c5a2ca128292098af247ee
Tami Olafunmiloye <tamiolaf@gmail.com>
2022-02-20 00:59:46 -0500

    Restart site and upgrade packages (#48)
    * run nom install
    
    * changes I think
    
    * restart site
    
    * add firebase files
    
    * fix other issues

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,19 +1,19 @@
-import { Link, Switch, useRouteMatch, Route }from 'react-router-dom';
+import { Link, Routes, useMatch, Route }from 'react-router-dom';
 import PlaygroundGame from './PlaygroundGame';
 import { SNAKE } from '../routes';
 
 function Playground() {
 
-    let { path, url } = useRouteMatch();
+    let { path, url } = useMatch();
 
     return (
         <div>
             <div>
                 <li><Link to={`${url}${SNAKE}`}>snake</Link></li>
             </div>
-            <Switch>
+            <Routes>
                 <Route path={`${path}/:gameName`} component={PlaygroundGame} />
-            </Switch>
+            </Routes>
         </div>
     )
 }
