83d76858148cb3a3388eec7e39c70dd57bd98c86
Tami Olafunmiloye <tamiolaf@gmail.com>
2022-03-19 13:24:03 -0400

    create portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,28 +1,38 @@
-import { Link, useMatch } from 'react-router-dom';
-// import { BUZZFEED, SEEFOOD, MIT_DCI, EQUALS_EQUALS, CODE2040 } from '../routes';
+import { Routes, Route } from 'react-router-dom';
+import { BUZZFEED, SEEFOOD, MIT_DCI, EQUALS_EQUALS, CODE2040, ARDUINO_IPOD } from '../routes';
 import Preview from './Preview';
+import BuzzFeed from './PortfolioItems/BuzzFeed';
+import SeeFood from './PortfolioItems/SeeFood';
+import MITDCI from './PortfolioItems/MITDCI'
+import EqualsEquals from './PortfolioItems/EqualsEquals';
+import Code2040 from './PortfolioItems/Code2040';
+import ArduinoIpod from './PortfolioItems/ArduinoIpod';
 import { data } from '../PreviewData.js';
 
+const ShowPreviews = () => {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      {data.map(item => <Preview key={item.key} {...item}/>)}
+    </div>
+  )
+}
 function Portfolio() {
 
-    let { url } = useMatch();
+  return (
+    <div>     
+      <Routes>
+        <Route path={'/'} element={ <ShowPreviews/>}/>
+        <Route path={BUZZFEED} element={<BuzzFeed/>}/>
+        <Route path={SEEFOOD} element={<SeeFood/>}/>
+        <Route path={EQUALS_EQUALS} element={<EqualsEquals/>}/>
+        <Route path={MIT_DCI} element={<MITDCI/>}/>
+        <Route path={CODE2040} element={<Code2040/>}/>
+        <Route path={ARDUINO_IPOD} element={<ArduinoIpod/>}/>
+      </Routes>
+    </div>
 
-    const showPreviews = data.map(previewData => (
-               <div key={previewData.id.toString()}>
-                    <Link style={{textDecoration: 'none', color: 'black'}} to={`${url}${previewData.route}`}>
-                        <Preview {...previewData}/>
-                    </Link>
-               </div>
-           )
-       )
-    
-    return (
-        
-        <div className="page-container">
-                <h1>Portfolio</h1>
-                <div>{showPreviews}</div>
-        </div>
-    )
+  )
 }
 
 export default Portfolio;
