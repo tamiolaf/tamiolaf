624b562ecb3c635dfc3a568ef61068389f745d68
Tami Olafunmiloye <tamiolaf@gmail.com>
2022-04-17 20:07:39 -0400

    add youtube embed

--- src/components/PortfolioItems/ArduinoIpod.js
+++ src/components/PortfolioItems/ArduinoIpod.js
@@ -4,8 +4,8 @@ const ArduinoIpod = () => {
   return (
     <div>
       <h1>{data.title}</h1>
+      <iframe width="560" height="315" src={data.externalLinks.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-  )
-}
+  )}
 
 export default ArduinoIpod;
