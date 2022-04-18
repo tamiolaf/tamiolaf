65981feb9bc5c4b52bd1ae0b8e9e7c5d8f6d7fa5
Tami Olafunmiloye <tamiolaf@gmail.com>
2022-04-17 23:53:40 -0400

    add image imports

--- src/components/PortfolioItems/SeeFood.js
+++ src/components/PortfolioItems/SeeFood.js
@@ -1,4 +1,32 @@
 import data from '../../copy/seefood.json'
+import Logo from '../../img/SeeFood/Logos/SeeFoodLogo.png';
+import AppsScriptScreenshot1 from '../../img/SeeFood/AppsScriptSite/screenshot1.png';
+import AppsScriptScreenshot2 from '../../img/SeeFood/AppsScriptSite/screenshot2.png';
+import StaticSiteScreenshot1 from '../../img/SeeFood/StaticSite/screenshot1.png';
+import StaticSiteScreenshot2 from '../../img/SeeFood/StaticSite/screenshot2.png';
+import StaticSiteScreenshot3 from '../../img/SeeFood/StaticSite/screenshot3.png';
+import StaticSiteScreenshot4 from '../../img/SeeFood/StaticSite/screenshot4.png';
+import StaticSiteScreenshot5 from '../../img/SeeFood/StaticSite/screenshot5.png';
+import StaticSiteScreenshot6 from '../../img/SeeFood/StaticSite/screenshot6.png';
+import StaticSiteScreenshot7 from '../../img/SeeFood/StaticSite/screenshot7.png';
+import WebAppScreenshot1 from '../../img/SeeFood/WebApp/screenshot1.png';
+import WebAppScreenshot2 from '../../img/SeeFood/WebApp/screenshot2.png';
+import WebAppScreenshot3 from '../../img/SeeFood/WebApp/screenshot3.png';
+import WebAppScreenshot4 from '../../img/SeeFood/WebApp/screenshot4.png';
+import WebAppScreenshot5 from '../../img/SeeFood/WebApp/screenshot5.png';
+import WebAppScreenshot6 from '../../img/SeeFood/WebApp/screenshot6.png';
+import WebAppScreenshot7 from '../../img/SeeFood/WebApp/screenshot7.png';
+import WebAppScreenshot8 from '../../img/SeeFood/WebApp/screenshot8.png';
+import WebAppScreenshot9 from '../../img/SeeFood/WebApp/screenshot9.png';
+import WebAppScreenshot10 from '../../img/SeeFood/WebApp/screenshot10.png';
+import WebAppScreenshot11 from '../../img/SeeFood/WebApp/screenshot11.png';
+import WebAppScreenshot12 from '../../img/SeeFood/WebApp/screenshot12.png';
+import WebAppScreenshot13 from '../../img/SeeFood/WebApp/screenshot13.png';
+import WebAppScreenshot14 from '../../img/SeeFood/WebApp/screenshot14.png';
+// import enterpreneurPic from '../../img/SeeFood/PeoplePics/enterpreneurSeeFood.jpg';
+// import soloGogglesPic from '../../img/SeeFood/PeoplePics/soloGogglesPic.jpg';
+// import groupGogglesPic from '../../img/SeeFood/PeoplePics/groupGogglesPic.jpg';
+// import chikfilaEventGif from '../../img/SeeFood/PeoplePics/chikfilaEvent.gif';
 
 const SeeFood = () => {
   return (
