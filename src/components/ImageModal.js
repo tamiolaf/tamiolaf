1f0a2ae085df24f484b3c41568519a58c5d40e3c
Tami Olafunmiloye <tamiolaf@gmail.com>
2022-11-09 12:54:54 -0500

    add image modal scss file and mobile breakpoint

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -1,35 +1,16 @@
+import '../scss/ImageModal.scss';
 
 const ImageModal = ({selectedImage, setSelectedImage}) => {
   return (
     selectedImage ? (
     <div
-      id='image-modal__background'
-      style={{
-        zIndex: 1,
-        width: '100vw',
-        height: '100vh',
-        position: 'fixed',
-        backgroundColor: 'rgba(0,0,0,0.75)',
-        top: 0,
-        left: 0,
-        display: 'flex',
-      }}
+      id='image-modal__container'
       onClick={e => {
-        if (e.target.id === 'image-modal__background') setSelectedImage(null);        
+        if (e.target.id === 'image-modal__container') setSelectedImage(null);        
       }}
     >
-   
       <img
-        id='image-modal__image'
-        style={{
-          zIndex: 2,
-          opacity: 1,
-          margin: '15% auto',
-          height: 'auto',
-          maxWidth: '90%'
-        }}
-        src={selectedImage}
-        alt=""/>
+        id='image-modal__image' src={selectedImage} alt=""/>
     </div>
     )
     :
