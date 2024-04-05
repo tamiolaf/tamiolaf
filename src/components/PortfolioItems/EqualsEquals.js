09ce93c95e1099398656403f2f382b7effea4a08
Tami Olafunmiloye <tamiolaf@gmail.com>
2024-04-04 23:44:13 -0400

    add completed Equals Equals copy and images(#82)
    * add summary text
    
    * add summary text
    
    * add equals eqials draft copy and import pictures
    
    * add equals equals overiew draft
    
    * add equals equals hackathon section draft
    
    * add completed Equals Equals copy and images

--- src/components/PortfolioItems/EqualsEquals.js
+++ src/components/PortfolioItems/EqualsEquals.js
@@ -1,13 +1,39 @@
-import equalsequalsLogo from '../../img/EqualsEquals/equalsEqualsLogo.jpg';
-// import podiumPic from '../../img/EqualsEquals/podiumPic.jpg';
-// import allWinnersPic from '../../img/EqualsEquals/allWinnersPic.jpg';
+import data from '../../copy/equals-equals.json';
+import PortfolioItem from '../PortfolioItem';
 
+import equalsEqualsLogo from '../../img/EqualsEquals/equalsEqualsLogo.jpg';
+import equalsEqualsLogoMini from '../../img/EqualsEquals/equalsEqualsLogoMini.png';
+import FirstPlacePic from '../../img/EqualsEquals/FirstPlacePic.jpg';
+import SecondPlacePic from '../../img/EqualsEquals/SecondPlacePic.jpg';
+import allWinnersPic from '../../img/EqualsEquals/allWinnersPic.jpg';
+import equals_equals_flyer from '../../img/EqualsEquals/equals_equals_flyer.png';
+import podiumPic from '../../img/EqualsEquals/podiumPic.jpg';
+import judgingPic1 from '../../img/EqualsEquals/judgingPic1.jpg';
+
+const galleryMap = new Map([
+  ["Overview", [
+    equalsEqualsLogo,
+    equals_equals_flyer,
+    equalsEqualsLogoMini,
+  ]],
+
+  ["Hackathon", [
+    podiumPic,
+    judgingPic1,
+    FirstPlacePic,
+    SecondPlacePic,
+    allWinnersPic,
+  ]]
+])
 const EqualsEquals = () => {
   return (
     <div>
-      <h1>Equals Equals</h1>
-      <h3>A hackathon for underrepresented minorities</h3>
-      <img src={equalsequalsLogo} alt=''></img>
+      <PortfolioItem data={data} galleryMap={galleryMap}/>
+      <p>
+        Here was our <a href="https://docs.google.com/presentation/d/1X7s1h0Um4QkyGGtf4HdQlxc7-MnPZ5AISoj1UMVKFCQ/"> presentation </a> to kick off the hackday.
+        And here's my <a href="https://docs.google.com/presentation/d/1hwNWNgbE4iO77YcHU0Y4fhiAlWnNTGy0C2G2Gm_FrQg/">presentation</a> for the Twitter Bot workshop (Twitter has changed dramatically and this may be outdated).
+        This <a href="https://www.reddit.com/r/UMD/comments/axhaq2/upcoming_free_hackathon_this_weekend_march_910/">reddit</a> post and <a href="https://equals-equals.devpost.com/">devpost</a> page served as marketing material for Equals Equals 2019.
+      </p>
     </div>
   )
 };
