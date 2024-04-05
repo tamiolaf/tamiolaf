949cbf1617fdb83ca10739dd56a634727eb7f25d
Tami Olafunmiloye <tamiolaf@gmail.com>
2024-04-05 00:30:57 -0400

    add-paragraph-functionality

--- src/components/PortfolioItemSection.js
+++ src/components/PortfolioItemSection.js
@@ -1,11 +1,13 @@
 import PortfolioItemGallery from './PortfolioItemGallery';
 
 const PortfolioItemSection = ({title, details, images}) => {
+  const showDetails = (details) => {
+    return details.join(' ').split("\n").map(str => <p>{str}</p>)
+  }
   return (
     <div>
-      {}
         <h2>{title}</h2>
-        <p>{details.join(' ')}</p>
+        {showDetails(details)}
         <PortfolioItemGallery images={images}/>
     </div>
   )
