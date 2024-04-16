ae384eab069370b7fd21e8db07866e51cc242d7f
Tami Olafunmiloye <tamiolaf@gmail.com>
2024-04-15 23:54:30 -0400

    start section and add pictures

--- src/components/PortfolioItems/Code2040.js
+++ src/components/PortfolioItems/Code2040.js
@@ -1,9 +1,22 @@
 import data from '../../copy/code2040.json';
+import PortfolioItem from '../PortfolioItem';
 
+import rooftopEvent from '../../img/Code2040/rooftopEvent.JPG';
+import careerFair from '../../img/Code2040/careerFair.jpg';
+import faceCollage from '../../img/Code2040/faceCollage.jpg';
+
+const galleryMap = new Map([
+  ["Overview", [
+    rooftopEvent,
+    careerFair,
+    faceCollage,
+  ]],
+])
 const Code2040 = () => {
   return (
     <div>
-      <h1>{data.title}</h1>
+      <PortfolioItem data={data} galleryMap={galleryMap}/>
+      {console.log(galleryMap)}
     </div>
   )
 }
